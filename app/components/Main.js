05d624b9b75be7e38cfe69a462a83ebd279cfc32
fuzzlemuttons <sebastianbender@gmail.com>
2015-08-08 21:56:25 +0300

    initial setup for webpack/react with react-hot-loader

---
+++ app/components/Main.js
@@ -0,0 +1,13 @@
+var React = require("react");
+
+var Main = React.createClass({
+   render: function(){
+       return (
+           <div>
+               Children sasa
+           </div>
+       )
+   }
+});
+
+module.exports = Main;
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+    devtool: "eval",
+    entry: [
+        'webpack-dev-server/client?http://localhost:3000', // WebpackDevServer host and port
+        'webpack/hot/only-dev-server',
+        "./app/index.js"
+    ],
+    output: {
+        path: path.join(__dirname, "public"),
+        filename: "bundle.js",
+        publicPath: "/public/"
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NoErrorsPlugin()
+    ],
+    module: {
+        loaders: [
+            {
+                test: /\.jsc?$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: "babel-loader!react-hot"
+            }
+        ]
+    }
+};
