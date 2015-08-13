cf16901e2c1187622b201f74b2b9454c922eaed7
fuzzlemuttons <sebastianbender@gmail.com>
2015-08-14 00:12:30 +0300

    Added Section components with dummy data, and Profile component.

--- app/components/Home.js
+++ app/components/Home.js
@@ -3,13 +3,13 @@
 var React = require('react');
 
 var Home = React.createClass({
- render: function() {
-   return (
-     <h2 className="text-center">
-         Search by Github Username Above
-     </h2>
-   );
- }
+    render: function () {
+        return (
+            <h2 className="text-center">
+                Search by Github Username Above
+            </h2>
+        );
+    }
 });
 
 module.exports = Home;
